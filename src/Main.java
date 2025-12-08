2403551a0ea2e238fd47ee5bf14581e50ebabfc6
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2025-12-08 16:56:54 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -401,10 +401,46 @@ Docker compose - позволяет запускать, останавливат
 
 
 
+================================
+
+        portainer
+
+
+            https://docs.portainer.io/start/install-ce/server/docker/linux
+            docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+            https://localhost:9443/
 
 
 
-3 55  !!!!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
